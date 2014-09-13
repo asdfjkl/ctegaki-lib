76ef31976fbe8b69fc97ce49d13e6aa2febfb11f
Dominik Klein <Dominik.Klein@jaist.ac.jp>
2014-09-14 02:50:26 +0900

    demo

--- demo/main.c
+++ demo/main.c
@@ -6,5 +6,23 @@ int main() {
 	setlocale(LC_ALL, "en_US.UTF-8");
 	kanjis data = load_data("data.dat");
 	kanji k;
-	
+	point p00;
+	point p01;
+	point p11;
+	point p10;
+
+	p00.x = 0;
+	p00.y = 0;
+	point[] stroke1 = { p00, p00, p00  };
+        printf("ok");
+	printf("\n");
+
+	add_stroke(&k,stroke1,3);
+	printf("ok");
+	printf("\n");
+	wchar_t *results = recognize_kanji(k,data);
+	for(int i=0;i<2;i++) { 
+		wprintf(L"%lc ",results[i]);
+	}
+	printf("\n");
 }
