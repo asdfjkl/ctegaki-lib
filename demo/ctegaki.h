93465337720f847b557561afc4526403d6f1366e
asdfjkl <asdfjkl@users.noreply.github.com>
2014-09-21 22:14:32 +0200

    removed unneccessary header files

--- demo/ctegaki.h
+++ demo/ctegaki.h
@@ -8,18 +8,10 @@
 #ifndef CTEGAKI_H
 #define	CTEGAKI_H
 
-#include "xml.h"
-#include "binary.h"
 #include "kanji.h"
 #include "kanjis.h"
-#include "globals.h"
 #include <wchar.h>
 #include <locale.h>
-#include "distance.h"
-#include "strokemap.h"
-#include "rasterize.h"
-#include "recognizer.h"
-#include "tinydir.h"
 
 wchar_t* recognize_kanji(kanji unknown, kanjis dataset);
 kanjis load_data(char* filename);
--- demo/xml.h
+++
@@ -1,6 +0,0 @@
-#include "kanji.h"
-#ifndef _XML_READER_H_
-#define _XML_READER_H_
-
-void read_xml_file(char *filename, kanji* k);
-#endif
